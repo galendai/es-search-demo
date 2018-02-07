34987fac9780a7c78c14117a1d1b19d5b148faf9
liubaicai <liushuai.baicai@hotmail.com>
2018-02-07 13:40:05 +0800

    test

--- db/article.js
+++ db/article.js
@@ -293,7 +293,13 @@ const importer = async function () {
                             }
                         }
                     }
-                });
+                })
+                    .then(() => {
+                        console.log('Connection has been established successfully.')
+                    })
+                    .catch((err) => {
+                        console.error('Unable to connect to the database:', err)
+                    });
             }
         } catch (err) {
             console.log(err)
